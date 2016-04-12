47ab58c7365dd665d8ee9761fc6a108a12528ee5
Uguisu32J <uguisu32j@gmail.com>
2016-04-13 01:47:00 +0900

    Check sent event
    MinecraftView doesn't have methods to responde control-character-key events.So when control-character-key event is sent,MinecraftView takes beep.Filtered key event not MinecraftView to receivecontrol-character-key events.

--- src/main/java/com/Axeryok/CocoaInput/CocoaInput.java
+++ src/main/java/com/Axeryok/CocoaInput/CocoaInput.java
@@ -29,7 +29,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class CocoaInput extends DummyModContainer
 {
     public static final String MODID = "CocoaInput";
-    public static final String VERSION = "3.0.1";
+    public static final String VERSION = "3.0.2";
     
     public CocoaInput(){
     	super(new ModMetadata());
