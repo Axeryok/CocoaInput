0cac724ee1a9594d96ea4b83c578736b4807da6c
Axer <info@axer.jp>
2016-04-13 01:47:00 +0900

    Check sent event
    MinecraftView doesn't have methods to responde control-character-key events.So when control-character-key event is sent,MinecraftView takes beep.Filtered key event not MinecraftView to receivecontrol-character-key events.

--- cocoainput/cocoainput/DataManager.m
+++ cocoainput/cocoainput/DataManager.m
@@ -29,7 +29,15 @@ static DataManager* instance = nil;
 }
 
 - (void)keyDown:(NSEvent*)theEvent {
-  if ([DataManager sharedManager].activeView != nil) {
+  //見づらすぎて改修しづらい
+  if ([DataManager sharedManager].activeView != // activeViewがnilな時  MarkedTextが無い状況での制御文字はtheEventを解釈させない
+          nil &&
+      ([[DataManager sharedManager] activeView].hasMark == YES ||
+       ([[DataManager sharedManager] activeView].hasMark == NO &&
+        (([[theEvent characters] characterAtIndex:0] != 0x7F &&
+          ([[theEvent characters] characterAtIndex:0] > 0x1B &&
+           [[theEvent characters] characterAtIndex:0] < 0xF700)) ||
+         [[theEvent characters] characterAtIndex:0] > 0xF8FF)))) {
     [[[DataManager sharedManager] activeView] interpretKeyEvents:@[ theEvent ]];
   }
   if ([[DataManager sharedManager] activeView].hasMark == NO &&
