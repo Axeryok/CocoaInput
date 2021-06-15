439ce7ac53fa40e18d20b5ca04ed5fc29bdc49d2
Uguisu32J <uguisu32j@gmail.com>
2021-06-16 03:15:20 +0900

    Update sign and book

--- src/main/java/jp/axer/cocoainput/wrapper/EditBookScreenWrapper.java
+++ src/main/java/jp/axer/cocoainput/wrapper/EditBookScreenWrapper.java
@@ -9,6 +9,7 @@ import jp.axer.cocoainput.plugin.IMEReceiver;
 import jp.axer.cocoainput.util.ModLogger;
 import jp.axer.cocoainput.util.PreeditFormatter;
 import jp.axer.cocoainput.util.Rect;
+import jp.axer.cocoainput.util.Tuple3;
 import jp.axer.cocoainput.util.WrapperUtil;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.screen.EditBookScreen;
@@ -22,6 +23,9 @@ public class EditBookScreenWrapper implements IMEReceiver {
     private int length = 0;
     private boolean hasMarkedText = false;
     private int lengthBeforeMarkedText;
+    
+    private boolean cursorVisible=true;
+    private int originalCursorPosition=0;
 
     public EditBookScreenWrapper(EditBookScreen field) {
         ModLogger.log("EditBookScreen init: " + field.hashCode());
@@ -32,39 +36,91 @@ public class EditBookScreenWrapper implements IMEReceiver {
 
     @Override
     public void insertText(String aString, int position1, int length1) {
+    	
         if (owner.isSigning) {
-            owner.title = new StringBuffer(owner.title).replace(lengthBeforeMarkedText,
-                    lengthBeforeMarkedText + length, aString).toString();
-            hasMarkedText = false;
-            length = 0;
+            if(!this.hasMarkedText) {
+            	originalCursorPosition=owner.titleEdit.getCursorPos();
+            }
+            this.hasMarkedText=false;
+            this.cursorVisible=true;
+            if(aString.length()==0) {
+            	owner.title = new StringBuffer(owner.title).replace(originalCursorPosition,originalCursorPosition + length, aString).toString();
+            	length=0;
+            	owner.titleEdit.setCursorPos(originalCursorPosition,true);
+            	owner.titleEdit.setSelectionRange(originalCursorPosition,originalCursorPosition);
+            	return;
+            }
+            length=0;
+            owner.titleEdit.setCursorPos(originalCursorPosition,true);
+            
         } else {
-            owner.setCurrentPageText(new StringBuffer(owner.getCurrentPageText()).replace(
-                    lengthBeforeMarkedText, lengthBeforeMarkedText + length, aString).toString());
-            hasMarkedText = false;
-            length = 0;
+        	if(!this.hasMarkedText) {
+            	originalCursorPosition=owner.pageEdit.getCursorPos();
+            }
+            this.hasMarkedText=false;
+            this.cursorVisible=true;
+            if(aString.length()==0) {
+            	owner.setCurrentPageText(new StringBuffer(owner.getCurrentPageText()).replace(originalCursorPosition,originalCursorPosition + length, aString).toString());
+            	length=0;
+            	owner.pageEdit.setCursorPos(originalCursorPosition,true);
+            	owner.pageEdit.setSelectionRange(originalCursorPosition,originalCursorPosition);
+            	return;
+            }
+            length=0;
+            owner.pageEdit.setCursorPos(originalCursorPosition,true);
+        	
+        	
         }
     }
 
     @Override
     public void setMarkedText(String aString, int position1, int length1, int position2, int length2) {
-        String str = PreeditFormatter.formatMarkedText(aString, position1, length1)._1();
-        if (owner.isSigning) {
-            if (hasMarkedText == false) {
-                hasMarkedText = true;
-                lengthBeforeMarkedText = owner.title.length();
-            }
-            owner.title = new StringBuffer(owner.title).replace(lengthBeforeMarkedText,
-                    lengthBeforeMarkedText + length, str).toString();
+    	if(owner.isSigning) {
+    		if(!this.hasMarkedText) {
+    			originalCursorPosition = owner.titleEdit.getCursorPos();
+    			hasMarkedText=true;
+    		}
+    		Tuple3<String, Integer, Boolean> formattedText = PreeditFormatter.formatMarkedText(aString, position1, length1);
+            String str = formattedText._1();
+            int caretPosition = formattedText._2()+4;//相対値
+            boolean hasCaret = formattedText._3();
+            owner.title = new StringBuffer(owner.title).replace(originalCursorPosition, originalCursorPosition + length, str).toString();
             length = str.length();
-        } else {
-            if (hasMarkedText == false) {
-                hasMarkedText = true;
-                lengthBeforeMarkedText = owner.getCurrentPageText().length();
+            if (hasCaret) {
+                this.cursorVisible = true;
+                owner.titleEdit.setCursorPos(originalCursorPosition + caretPosition,true);
+                owner.titleEdit.setSelectionRange(originalCursorPosition + caretPosition,originalCursorPosition + caretPosition);
+            } else {
+                this.cursorVisible = false;
+                //TODO need to enable -> owner.frame = 6;
+                owner.titleEdit.setCursorPos(originalCursorPosition,true);
+                //owner.selectionEnd=owner.cursorPosition;
             }
-            owner.setCurrentPageText(new StringBuffer(owner.getCurrentPageText()).replace(
-                    lengthBeforeMarkedText, lengthBeforeMarkedText + length, str).toString());
+    	}
+    	else {
+    		if(!this.hasMarkedText) {
+    			originalCursorPosition = owner.pageEdit.getCursorPos();
+    			hasMarkedText=true;
+    		}
+    		Tuple3<String, Integer, Boolean> formattedText = PreeditFormatter.formatMarkedText(aString, position1, length1);
+            String str = formattedText._1();
+            int caretPosition = formattedText._2()+4;//相対値
+            boolean hasCaret = formattedText._3();
+            owner.setCurrentPageText(new StringBuffer(owner.getCurrentPageText()).replace(originalCursorPosition, originalCursorPosition + length, str).toString());
             length = str.length();
-        }
+            if (hasCaret) {
+                this.cursorVisible = true;
+                owner.pageEdit.setCursorPos(originalCursorPosition + caretPosition,true);
+                owner.pageEdit.setSelectionRange(originalCursorPosition + caretPosition,originalCursorPosition + caretPosition);
+            } else {
+                this.cursorVisible = false;
+                //TODO need to enable -> owner.frame = 6;
+                owner.pageEdit.setCursorPos(originalCursorPosition,true);
+                owner.pageEdit.setSelectionRange(originalCursorPosition,originalCursorPosition);
+                //owner.selectionEnd=owner.cursorPosition;
+            }
+    	}
+    	
     }
 
     @Override
@@ -77,7 +133,7 @@ public class EditBookScreenWrapper implements IMEReceiver {
         }
         if (owner.isSigning) {
             return new Rect(
-                    (fontRendererObj.width(owner.title) / 2 + ((owner.width - 192) / 2) + 36 + (116 - 0) / 2),
+                    (fontRendererObj.width(owner.title.substring(0, originalCursorPosition)) / 2 + ((owner.width - 192) / 2) + 36 + (116 - 0) / 2),
                     (50 + fontRendererObj.lineHeight),
                     0,
                     0
--- src/main/java/jp/axer/cocoainput/wrapper/EditSignScreenWrapper.java
+++ src/main/java/jp/axer/cocoainput/wrapper/EditSignScreenWrapper.java
@@ -6,6 +6,7 @@ import jp.axer.cocoainput.plugin.IMEReceiver;
 import jp.axer.cocoainput.util.ModLogger;
 import jp.axer.cocoainput.util.PreeditFormatter;
 import jp.axer.cocoainput.util.Rect;
+import jp.axer.cocoainput.util.Tuple3;
 import jp.axer.cocoainput.util.WrapperUtil;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.screen.EditSignScreen;
@@ -17,6 +18,8 @@ public class EditSignScreenWrapper implements IMEReceiver {
     private int length = 0;
     private boolean hasMarkedText = false;
     private int lengthBeforeMarkedText;
+    boolean cursorVisible = true;
+    int originalCursorPosition=0;
 
     public EditSignScreenWrapper(EditSignScreen field) {
         ModLogger.debug("EditSignScreen init: " + field.hashCode());
@@ -27,30 +30,61 @@ public class EditSignScreenWrapper implements IMEReceiver {
 
     @Override
     public void insertText(String aString, int position1, int length1) {
-        String text = owner.sign.getMessage(owner.line).getString();
-        String newEditLine = new StringBuffer(text).replace(lengthBeforeMarkedText, lengthBeforeMarkedText + length, aString)
-                .toString();
-        owner.sign.setMessage(owner.line, new StringTextComponent(newEditLine));
-        String[] util = owner.messages;
+    	if(!hasMarkedText) {
+    		originalCursorPosition=owner.signField.getCursorPos();
+    	}
+    	hasMarkedText=false;
+    	cursorVisible=true;
+    	String text = owner.sign.getMessage(owner.line).getString();
+    	if(aString.length()==0) {
+    		String newEditLine = new StringBuffer(text).replace(originalCursorPosition, originalCursorPosition + length, "").toString();
+    		owner.sign.setMessage(owner.line, new StringTextComponent(newEditLine));
+    		length=0;
+    		owner.signField.setCursorPos(originalCursorPosition,true);
+    		owner.signField.setSelectionRange(originalCursorPosition,originalCursorPosition);
+    		String[] util = owner.messages;
+            util[owner.line] = newEditLine;
+    		return;
+    	}
+    	String newEditLine = new StringBuffer(text).replace(originalCursorPosition, originalCursorPosition + length, aString.substring(0,aString.length())).toString();
+    	owner.sign.setMessage(owner.line, new StringTextComponent(newEditLine));
+    	String[] util = owner.messages;
         util[owner.line] = newEditLine;
-        hasMarkedText = false;
-        length = 0;
+    	length = 0;
+		owner.signField.setCursorPos(originalCursorPosition+aString.length(),true);
+		
+		
+
     }
 
     @Override
     public void setMarkedText(String aString, int position1, int length1, int position2, int length2) {
-        String str = PreeditFormatter.formatMarkedText(aString, position1, length1)._1();
-        String text = owner.sign.getMessage(owner.line).getString();
-        if (hasMarkedText == false) {
-            hasMarkedText = true;
-            lengthBeforeMarkedText = text.length();
-        }
-        String newEditLine = new StringBuffer(text).replace(lengthBeforeMarkedText, lengthBeforeMarkedText + length, aString)
-                .toString();
-        owner.sign.setMessage(owner.line, new StringTextComponent(newEditLine));
-        String[] util = owner.messages;
+    	if(!this.hasMarkedText) {
+    		this.originalCursorPosition=owner.signField.getCursorPos();
+    		this.hasMarkedText=true;
+    	}
+    	Tuple3<String,Integer,Boolean> formattedText=PreeditFormatter.formatMarkedText(aString, position1, length1);
+    	String str = formattedText._1();
+    	int caretPosition=formattedText._2()+4;
+    	boolean hasCaret = formattedText._3();
+    	String text = owner.sign.getMessage(owner.line).getString();
+    	String newEditLine = new StringBuffer(text).replace(originalCursorPosition, originalCursorPosition + length, str).toString();
+    	owner.sign.setMessage(owner.line, new StringTextComponent(newEditLine));
+    	String[] util = owner.messages;
         util[owner.line] = newEditLine;
-        length = str.length();
+    	length=str.length();
+    	if(hasCaret) {
+    		this.cursorVisible=true;
+    		owner.signField.setCursorPos(originalCursorPosition+caretPosition,true);
+    		owner.signField.setSelectionRange(originalCursorPosition+caretPosition,originalCursorPosition+caretPosition);
+    	}
+    	else {
+    		this.cursorVisible=false;
+    		/*TODO need to enable -> owner.frame=6*/
+    		owner.signField.setCursorPos(originalCursorPosition,true);
+    		owner.signField.setSelectionRange(originalCursorPosition,originalCursorPosition);
+    		
+    	}
     }
 
     @Override
@@ -66,7 +100,8 @@ public class EditSignScreenWrapper implements IMEReceiver {
             y += 30;
         }
         return new Rect(
-                owner.width / 2 + fontRendererObj.width(owner.sign.getMessage(owner.line).getString()) / 2,
+        		owner.width/2+fontRendererObj.width(owner.sign.getMessage(owner.line).toString().substring(0, originalCursorPosition))/2,
+//                owner.width / 2 + fontRendererObj.width(owner.sign.getMessage(owner.line).getString()) / 2,
                 y,
                 0,
                 0
