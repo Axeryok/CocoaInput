41c59bf19ddcc040a11b08fed931a2bb3666f146
Uguisu32J <uguisu32j@gmail.com>
2021-06-16 20:22:41 +0900

    Optimize IMEReceiver

--- src/main/java/jp/axer/cocoainput/CocoaInput.java
+++ src/main/java/jp/axer/cocoainput/CocoaInput.java
@@ -13,74 +13,83 @@ import org.apache.commons.io.IOUtils;
 import com.sun.jna.Platform;
 
 import jp.axer.cocoainput.arch.darwin.DarwinController;
+import jp.axer.cocoainput.arch.dummy.DummyController;
 import jp.axer.cocoainput.arch.win.WinController;
 import jp.axer.cocoainput.arch.x11.X11Controller;
-import jp.axer.cocoainput.arch.dummy.DummyController;
 import jp.axer.cocoainput.plugin.CocoaInputController;
 import jp.axer.cocoainput.util.ModLogger;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod("cocoainput")
 public class CocoaInput {
-    private static CocoaInputController controller;
+	private static CocoaInputController controller;
 
-    public CocoaInput() throws Exception {
-//        ModList.get().getModFileById("cocoainput").getConfig();
-        if(Platform.isMac()) {
-            CocoaInput.applyController(new DarwinController());
-        }
-	else if(Platform.isWindows()) {
-	    CocoaInput.applyController(new WinController());
-        }
-	else if(Platform.isX11()){
-	    CocoaInput.applyController(new X11Controller());
-        }
-        else{
-	    ModLogger.log("CocoaInput cannot find appropriate Controller in running OS.");
-            CocoaInput.applyController(new DummyController());
-        }
-        MinecraftForge.EVENT_BUS.register(this);
-        ModLogger.log("CocoaInput has been initialized.");
-    }
+	public CocoaInput() throws Exception {
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+		MinecraftForge.EVENT_BUS.register(this);
 
-    public static double getScreenScaledFactor() {
-    	return Minecraft.getInstance().getWindow().getGuiScale();
-    }
+	}
 
-    public static void applyController(CocoaInputController controller) throws IOException {
-        CocoaInput.controller = controller;
-        ModLogger.log("CocoaInput is now using controller:" + controller.getClass().toString());
-    }
+	private void setup(final FMLCommonSetupEvent event) {
+		try {
+			if (Platform.isMac()) {
+				CocoaInput.applyController(new DarwinController());
+			} else if (Platform.isWindows()) {
+				CocoaInput.applyController(new WinController());
+			} else if (Platform.isX11()) {
+				CocoaInput.applyController(new X11Controller());
+			} else {
+				ModLogger.log("CocoaInput cannot find appropriate Controller in running OS.");
+				CocoaInput.applyController(new DummyController());
+			}
+			ModLogger.log("CocoaInput has been initialized.");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-    public static CocoaInputController getController() {
-        return CocoaInput.controller;
-    }
+	public static double getScreenScaledFactor() {
+		return Minecraft.getInstance().getWindow().getGuiScale();
+	}
 
-    public static void copyLibrary(String libraryName, String libraryPath) throws IOException {
-        InputStream libFile;
-        try {//Modファイルを検出し、jar内からライブラリを取り出す
-            ZipFile jarfile = new ZipFile(ModList.get().getModFileById("cocoainput").getFile().getFilePath().toString());
-            libFile = jarfile.getInputStream(new ZipEntry(libraryPath));
-        } catch (FileNotFoundException e) {//存在しない場合はデバッグモードであるのでクラスパスからライブラリを取り出す
-            ModLogger.log("Couldn't get library path. Is this debug mode?'");
-            libFile = ClassLoader.getSystemResourceAsStream(libraryPath);
-        }
-        File nativeDir = new File(Minecraft.getInstance().gameDirectory.getAbsolutePath().concat("/native"));
-        File copyLibFile = new File(Minecraft.getInstance().gameDirectory.getAbsolutePath().concat("/native/" + libraryName));
-        try {
-            nativeDir.mkdir();
-            FileOutputStream fos = new FileOutputStream(copyLibFile);
-            copyLibFile.createNewFile();
-            IOUtils.copy(libFile, fos);
-            fos.close();
-        } catch (IOException e1) {
-            ModLogger.error("Attempted to copy library to ./native/" + libraryName + " but failed.");
-            throw e1;
-        }
-        System.setProperty("jna.library.path", nativeDir.getAbsolutePath());
-        ModLogger.log("CocoaInput has copied library to native directory.");
-    }
+	public static void applyController(CocoaInputController controller) throws IOException {
+		CocoaInput.controller = controller;
+		ModLogger.log("CocoaInput is now using controller:" + controller.getClass().toString());
+	}
+
+	public static CocoaInputController getController() {
+		return CocoaInput.controller;
+	}
+
+	public static void copyLibrary(String libraryName, String libraryPath) throws IOException {
+		InputStream libFile;
+		try {//Modファイルを検出し、jar内からライブラリを取り出す
+			ZipFile jarfile = new ZipFile(
+					ModList.get().getModFileById("cocoainput").getFile().getFilePath().toString());
+			libFile = jarfile.getInputStream(new ZipEntry(libraryPath));
+		} catch (FileNotFoundException e) {//存在しない場合はデバッグモードであるのでクラスパスからライブラリを取り出す
+			ModLogger.log("Couldn't get library path. Is this debug mode?'");
+			libFile = ClassLoader.getSystemResourceAsStream(libraryPath);
+		}
+		File nativeDir = new File(Minecraft.getInstance().gameDirectory.getAbsolutePath().concat("/native"));
+		File copyLibFile = new File(
+				Minecraft.getInstance().gameDirectory.getAbsolutePath().concat("/native/" + libraryName));
+		try {
+			nativeDir.mkdir();
+			FileOutputStream fos = new FileOutputStream(copyLibFile);
+			copyLibFile.createNewFile();
+			IOUtils.copy(libFile, fos);
+			fos.close();
+		} catch (IOException e1) {
+			ModLogger.error("Attempted to copy library to ./native/" + libraryName + " but failed.");
+			throw e1;
+		}
+		System.setProperty("jna.library.path", nativeDir.getAbsolutePath());
+		ModLogger.log("CocoaInput has copied library to native directory.");
+	}
 }
--- src/main/java/jp/axer/cocoainput/plugin/IMEReceiver.java
+++ src/main/java/jp/axer/cocoainput/plugin/IMEReceiver.java
@@ -1,14 +1,79 @@
 package jp.axer.cocoainput.plugin;
 
-
+import jp.axer.cocoainput.util.PreeditFormatter;
 import jp.axer.cocoainput.util.Rect;
+import jp.axer.cocoainput.util.Tuple3;
 
-public interface IMEReceiver{
-    void insertText(String aString,int position1,int length1);//確定文字列 現状aString以外の引数は意味をなしてない
-    /*
-     * position1 length1は下線と強調変換のため必須 position2 length2は意味をなしてない
-     * positionの位置から文字数lengthの範囲という意味
-     */
-    void setMarkedText(String aString,int position1,int length1,int position2,int length2);//
-    Rect getRect();
+public abstract class IMEReceiver {
+
+	private int length = 0;
+	protected boolean cursorVisible = true;
+	private boolean preeditBegin = false;
+	protected int originalCursorPosition = 0;
+
+	/*
+	 * position1 length1は下線と強調変換のため必須 position2 length2は意味をなしてない
+	 * positionの位置から文字数lengthの範囲という意味
+	 */
+	public void insertText(String aString, int position1, int length1) {//確定文字列 現状aString以外の引数は意味をなしてない
+		if (!preeditBegin) {
+			originalCursorPosition = this.getCursorPos();
+		}
+		preeditBegin = false;
+		cursorVisible = true;
+		if (aString.length() == 0) {
+			this.setText((new StringBuffer(this.getText()))
+					.replace(originalCursorPosition, originalCursorPosition + length, "").toString());
+			length = 0;
+			this.setCursorPos(originalCursorPosition);
+			this.setSelectionPos(originalCursorPosition);
+			return;
+		}
+		this.setText((new StringBuffer(this.getText()))
+				.replace(originalCursorPosition, originalCursorPosition + length,
+						aString.substring(0, aString.length()))
+				.toString());
+		length = 0;
+		this.setCursorPos(originalCursorPosition + aString.length());
+		//owner.selectionEnd = owner.cursorPosition;
+	}
+
+	public void setMarkedText(String aString, int position1, int length1, int position2, int length2) {
+		if (!preeditBegin) {
+			originalCursorPosition = this.getCursorPos();
+			preeditBegin = true;
+		}
+		Tuple3<String, Integer, Boolean> formattedText = PreeditFormatter.formatMarkedText(aString, position1, length1);
+		String str = formattedText._1();
+		int caretPosition = formattedText._2() + 4;//相対値
+		boolean hasCaret = formattedText._3();
+		this.setText((new StringBuffer(this.getText()))
+				.replace(originalCursorPosition, originalCursorPosition + length, str).toString());
+		length = str.length();
+		if (hasCaret) {
+			this.cursorVisible = true;
+			this.setCursorPos(originalCursorPosition + caretPosition);
+			this.setSelectionPos(originalCursorPosition + caretPosition);
+		} else {
+			this.cursorVisible = false;
+			this.setCursorInvisible();
+			this.setCursorPos(originalCursorPosition);
+			this.setSelectionPos(originalCursorPosition);
+			//owner.selectionEnd=owner.cursorPosition;
+		}
+	}
+
+	public  abstract Rect getRect();
+
+	abstract protected void setText(String text);
+
+	abstract protected String getText();
+
+	abstract protected void setCursorInvisible();
+
+	abstract protected int getCursorPos();
+
+	abstract protected void setCursorPos(int p);
+
+	abstract protected void setSelectionPos(int p);
 }
