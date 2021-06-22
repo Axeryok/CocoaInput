b634fcc16c120b00afd3b174696a6b1549566552
Axeryok <info@axer.jp>
2021-06-23 01:33:33 +0900

    Update cursor visible on sign and book

--- src/main/java/jp/axer/cocoainput/mixin/EditSignScreenMixin.java
+++ src/main/java/jp/axer/cocoainput/mixin/EditSignScreenMixin.java
@@ -1,8 +1,10 @@
 package jp.axer.cocoainput.mixin;
 
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import jp.axer.cocoainput.wrapper.EditSignScreenWrapper;
@@ -15,5 +17,10 @@ public class EditSignScreenMixin {
 	 @Inject(method="init",at=@At("RETURN"))
 	 private void init(CallbackInfo ci) {
 		 wrapper = new EditSignScreenWrapper((EditSignScreen)(Object)this);
+	 }
+	 
+	 @Redirect(method="tick",at = @At(value="FIELD", target="Lnet/minecraft/client/gui/screen/EditSignScreen;frame:I",opcode=Opcodes.PUTFIELD))
+	 private void injectCurosr(EditSignScreen esc,int n) {
+		 esc.frame=wrapper.renewCursorCounter();
 	 }
 }
